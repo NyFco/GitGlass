533a3b37142b9aaee33dd5928cc22f71c6cc3f09
Yazdan Vahidi Sharbaaf <yazdanvahidi@yahoo.com>
2024-09-10 01:04:24 +0330

    feat: add useAuth hook

---
+++ hooks/useAuth.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+import { useCallback, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+const useAuth = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const checkAuth = useCallback(() => {
+    return new Promise((resolve, reject) => {
+      const code = searchParams.get("code");
+      const accessToken = localStorage.getItem("access_token");
+
+      if (!accessToken) {
+        setIsAuthenticated(false);
+        if (!code) {
+          router.replace("/login");
+        } else {
+          axios
+            .post(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth`, {
+              code,
+            })
+            .then((response) => {
+              localStorage.setItem("access_token", response.data.accessToken);
+              setIsAuthenticated(true);
+              resolve(response.data.accessToken);
+            })
+            .catch((err) => {
+              return reject(err);
+            });
+        }
+      }
+    });
+  }, [router, searchParams]);
+
+  return {
+    isAuthenticated,
+    checkAuth,
+  };
+};
+
+export default useAuth;
